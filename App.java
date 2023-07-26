63b2d1a9834bd6f53a257988b6b34cba7223a292
duahau2130 <auditionkaka@gmail.com>
2023-07-26 22:32:03 +0700

    init project

---
+++ App.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("nhap ma sinh vien: ");
+        long maSingVien = sc.nextLong()
+    }
+}
